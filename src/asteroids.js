b8cc31509d424cb8c1715d06e429713e8c4fd86e
John Conover <111jbcon@gmail.com>
2014-11-12 23:39:45 -0500

    ship in its own class file, changed ship back to gray

--- src/asteroids.js
+++ src/asteroids.js
@@ -1,22 +1,6 @@
 
 var gl;
-var points;
-var colors = [];
-var theta = [0,0,0];
-var lastTheta = [0,0,0];
-var thetaLoc;
-
-var pos = vec2(0,0);
-var posLoc;
-var thrustOn = false;
-var thrustForce = .05;
-var rotateSpeed = 3;
-var vel = vec2(0,0);
-var deceleration = .0005;
-
-
-//up, down, left, right
-var moveVec = [false, false, false, false];
+var ship;
 
 window.onload = function init()
 {
@@ -25,18 +9,6 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var vertices = e_points;
-
-
-    points = toVec3(vertices);
-
-    for (var i = 0; i < points.length; i++){
-        for (var j = 0; j < points[i].length; j++){
-            points[i][j] /= 5;
-        }
-        colors.push(vec4(Math.abs(points[i][0]), Math.abs(points[i][1]), Math.abs(points[i][2]), 1.0));
-    }
-
     //
     //  Configure WebGL
     //
@@ -44,150 +16,75 @@ window.onload = function init()
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
     gl.enable(gl.DEPTH_TEST);
     
-    //  Load shaders and initialize attribute buffers
-    
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
 
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    ship = new Ship();
 
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
 
     
-    // Load the data into the GPU
+    /*// Load the data into the GPU
     
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(ship.points), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
     
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    gl.enableVertexAttribArray( vPosition );*/
 
 
     //register user input with keyboard
     window.addEventListener("keydown", function() {
         switch(event.keyCode){
             case 87:     //w
-                moveVec[0] = true;
+                ship.moveVec[0] = true;
                 break;
             case 83:     //s
-                moveVec[1] = true;
+                ship.moveVec[1] = true;
                 break;
             case 65:     //a
-                moveVec[2] = true;
+                ship.moveVec[2] = true;
                 break;
             case 68:     //d
-                moveVec[3] = true;
+                ship.moveVec[3] = true;
                 break;
             case 16:     //shift
-                thrustOn = true;
+                ship.thrustOn = true;
         }
     });
 
     window.addEventListener("keyup", function() {
         switch(event.keyCode){
             case 87:     //w
-                moveVec[0] = false;
+                ship.moveVec[0] = false;
                 break;
             case 83:     //s
-                moveVec[1] = false;
+                ship.moveVec[1] = false;
                 break;
             case 65:     //a
-                moveVec[2] = false;
+                ship.moveVec[2] = false;
                 break;
             case 68:     //d
-                moveVec[3] = false;
+                ship.moveVec[3] = false;
                 break;
             case 16:     //shift
-                thrustOn = false;
+                ship.thrustOn = false;
         }
     });
-
-    
-    thetaLoc = gl.getUniformLocation(program, "theta");
-    posLoc = gl.getUniformLocation(program, "pos");
-
     render();
 };
 
 
 function render() {
-	rotateShip();
-    moveShip();
-    thrust();
+	ship.rotate();
+    ship.move();
+    ship.thrust();
 
-    gl.uniform2fv(posLoc, pos);
-	gl.uniform3fv(thetaLoc, theta);
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, points.length );
+    ship.render();
     window.requestAnimFrame(render);
 }
 
-function rotateShip(){
-	if (moveVec[2]){
-        theta[2] += rotateSpeed;
-        console.log("LEFT!!");
-    }
-    if (moveVec[3]){
-        theta[2] -= rotateSpeed;
-        console.log("RIGHT!!"); 
-    }
-    if (theta[2] >= 360){
-        theta[2] -= 360;
-    }
-}
-
-function moveShip(){
-    pos[0] -= vel[0];
-    pos[1] -= vel[1];
-
-    if (vel[0] > 0){
-        vel[0] -= deceleration * Math.cos(radians(90 + lastTheta[2]));
-        if (vel[0] < 0){
-            vel[0] = 0;
-        }
-    }
-    else if (vel[0] < 0){
-        vel[0] -= deceleration * Math.cos(radians(90 + lastTheta[2]));
-        if (vel[0] > 0){
-            vel[0] = 0;
-        }
-    }
-    else{
-        vel[0] = 0;
-    }
-    if (vel[1] > 0){
-        vel[1] -= deceleration * Math.sin(radians(90 + lastTheta[2]));
-        if (vel[1] < 0){
-            vel[1] = 0;
-        }
-    }
-    else if (vel[1] < 0){
-        vel[1] -= deceleration * Math.sin(radians(90 + lastTheta[2]));
-        if (vel[1] > 0){
-            vel[1] = 0;
-        }
-    }
-    console.log(vel);
-}
-
-function thrust(){
-    if (thrustOn){
-        /* phase shift by -90 degrees since the ship 
-        starts facing in -y direction 
-        I could use sin and -cos but that looks counterintuitive */
-        vel[0] = thrustForce * Math.cos(radians(90 + theta[2]));
-        vel[1] = thrustForce * Math.sin(radians(90 + theta[2]));
-        lastTheta = theta.slice(0);
-    }
-}
 
 function toVec3(vertices){
     points = new Array();
---
+++ src/ship.js
@@ -0,0 +1,107 @@
+function Ship() {
+	//graphics and initialization
+	this.points = toVec3(e_points);
+	this.colors = [];
+
+	for (var i = 0; i < this.points.length; i++){
+        this.colors.push(vec4(0.5, 0.5, 0.5, 1), 1.0);
+    }
+
+	this.program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( this.program );
+
+    this.cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(this.colors), gl.STATIC_DRAW);
+
+    this.vColor = gl.getAttribLocation(this.program, "vColor");
+    gl.vertexAttribPointer(this.vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(this.vColor);
+
+	this.vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(this.points), gl.DYNAMIC_DRAW );
+    
+    this.vPosition = gl.getAttribLocation( this.program, "vPosition" );
+    gl.vertexAttribPointer( this.vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( this.vPosition );
+
+
+	//physics stuff
+	this.theta = [0,0,0];
+	this.lastTheta = [0,0,0];
+	this.thetaLoc = gl.getUniformLocation(this.program, "theta");
+	this.pos = vec2(0,0);
+	this.posLoc = gl.getUniformLocation(this.program, "pos");;
+	this.thrustOn = false;
+	this.thrustForce = .1;
+	this.rotateSpeed = 3;
+	this.vel = vec2(0,0);
+	this.deceleration = .0005;
+	this.moveVec = [false, false, false, false];
+
+	this.rotate = function (){
+		if (this.moveVec[2]){
+	        this.theta[2] += this.rotateSpeed;
+	        console.log("LEFT!!");
+	    }
+	    if (this.moveVec[3]){
+	        this.theta[2] -= this.rotateSpeed;
+	        console.log("RIGHT!!"); 
+	    }
+	    if (this.theta[2] >= 360){
+	        this.theta[2] -= 360;
+	    }
+	}
+
+	this.move = function(){
+	    this.pos[0] -= this.vel[0];
+	    this.pos[1] -= this.vel[1];
+
+	    if (this.vel[0] > 0){
+	        this.vel[0] -= this.deceleration * Math.cos(radians(90 + this.lastTheta[2]));
+	        if (this.vel[0] < 0){
+	            this.vel[0] = 0;
+	        }
+	    }
+	    else if (this.vel[0] < 0){
+	        this.vel[0] -= this.deceleration * Math.cos(radians(90 + this.lastTheta[2]));
+	        if (this.vel[0] > 0){
+	            this.vel[0] = 0;
+	        }
+	    }
+	    if (this.vel[1] > 0){
+	        this.vel[1] -= this.deceleration * Math.sin(radians(90 + this.lastTheta[2]));
+	        if (this.vel[1] < 0){
+	            this.vel[1] = 0;
+	        }
+	    }
+	    else if (this.vel[1] < 0){
+	        this.vel[1] -= this.deceleration * Math.sin(radians(90 + this.lastTheta[2]));
+	        if (this.vel[1] > 0){
+	            this.vel[1] = 0;
+	        }
+	    }
+	    console.log(this.vel);
+	}
+
+	this.thrust = function(){
+	    if (this.thrustOn){
+	        /* phase shift by -90 degrees since the ship 
+	        starts facing in -y direction 
+	        I could use sin and -cos but that looks counterintuitive */
+	        this.vel[0] = this.thrustForce * Math.cos(radians(90 + this.theta[2]));
+	        this.vel[1] = this.thrustForce * Math.sin(radians(90 + this.theta[2]));
+	        this.lastTheta = this.theta.slice(0);
+	    }
+	}
+
+	this.render = function(){
+		gl.uniform2fv(this.posLoc, this.pos);
+		gl.uniform3fv(this.thetaLoc, this.theta);
+	    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+	    gl.drawArrays( gl.TRIANGLES, 0, this.points.length );
+	}
+
+
+}
